19fb5bff883b712e0e3f287be5c73fb4367d158a
Victor Nikolaenko <64604542+vnikolaenko-dev@users.noreply.github.com>
2026-01-18 14:18:32 +0300

    Delete src directory

--- src/main/java/github/vnikolaenko/jarch/generator/auxiliary/Relation.java
+++
@@ -1,8 +0,0 @@
-package github.vnikolaenko.jarch.generator.auxiliary;
-
-import lombok.Data;
-
-@Data
-public class Relation {
-    private TypeOfRelation typeOfRelation;
-}
--- src/main/java/github/vnikolaenko/jarch/generator/auxiliary/TypeOfRelation.java
+++
@@ -1,8 +0,0 @@
-package github.vnikolaenko.jarch.generator.auxiliary;
-
-public enum TypeOfRelation {
-    ONE_TO_ONE,
-    ONE_TO_MANY,
-    MANY_TO_ONE,
-    MANY_TO_MANY
-}
